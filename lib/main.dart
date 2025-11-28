4678497f1cbfe6bd5f178fee22c61792e452d8e6
up2212102 <156660141+up2212102@users.noreply.github.com>
2025-11-28 14:53:26 +0000

    Implemented Snackbar Confirmation Message

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,12 @@ class _OrderScreenState extends State<OrderScreen> {
       String confirmationMessage =
           'Added $_quantity $sizeText ${sandwich.name} sandwich(es) on ${_selectedBreadType.name} bread to cart';
 
-      debugPrint(confirmationMessage);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(confirmationMessage),
+          duration: const Duration(seconds: 2),
+        ),
+      );
     }
   }
 
